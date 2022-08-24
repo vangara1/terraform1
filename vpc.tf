a9aaa5cb72871780f8b055807c64ea8ee721b661
vangara1 <vangarasandeep1996@gmail.com>
2022-08-24 09:49:13 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -19,6 +19,14 @@ resource "aws_subnet" "redflag-sub" {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
