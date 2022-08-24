38f9556b33943f8a3799a7d5ec612f53bba6b78a
vangara1 <vangarasandeep1996@gmail.com>
2022-08-24 10:03:36 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -11,8 +11,8 @@ resource "aws_subnet" "red-sub" {
   vpc_id                                         = aws_vpc.redflag.id
   cidr_block                                     = "190.0.0.0/17"
   availability_zone                              = "us-east-1b"
-  enable_resource_name_dns_aaaa_record_on_launch = true
-  enable_resource_name_dns_a_record_on_launch    = true
+#  enable_resource_name_dns_aaaa_record_on_launch = true
+#  enable_resource_name_dns_a_record_on_launch    = true
 
   tags = {
     Name = "redflag-sub"
