fee45899bea11b00595c2355d1639338239a95e0
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 13:32:43 +0530

    terraform

--- instance.tf
+++ instance.tf
@@ -4,7 +4,11 @@ resource "aws_iam_instance_profile" "sandy_profile" {
 }
 
 
-data "aws_launch_template" "default" {
-  name = "centos"
-  id = "lt-06a0f28ee3faf1441"
+
+resource "aws_instance" "role" {
+  ami           = "ami-002070d43b0a4f171"
+  instance_type = "t2.micro"
+  iam_instance_profile = "sandy_profile"
+
 }
+
