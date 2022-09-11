92ee878e98f2c326c9d89714494c9e61e0a626b3
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 15:38:34 +0530

    terraform

--- role/instance.tf
+++ role/instance.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "role" {
   ami                  = "ami-002070d43b0a4f171"
   instance_type        = "t2.micro"
   iam_instance_profile = "sandy_profile"
-
+  key_name             = "key"
 
   tags = {
     Name = "role"
@@ -16,7 +16,7 @@ resource "aws_instance" "role" {
 }
 resource "aws_key_pair" "key" {
   key_name   = "key"
-  public_key = tls_private_key.rsa.public_key_openssh
+  public_key = tls_private_key.rsa.public_key_pem
 }
 
 resource "tls_private_key" "rsa" {
