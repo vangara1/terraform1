75c9ee67c7c5dd3fa227a4cde5b9f2f7100bacd0
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 13:50:56 +0530

    terraform

--- role/instance.tf
+++ role/instance.tf
@@ -4,11 +4,12 @@ resource "aws_iam_instance_profile" "sandy_profile" {
 }
 
 
-
 resource "aws_instance" "role" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
   iam_instance_profile = "sandy_profile"
 
+  tags = {
+    Name = "role"
+  }
 }
-
