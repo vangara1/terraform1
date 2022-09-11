5be1f1332fd32c81c59907e64beee0965f9d87c5
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 13:31:38 +0530

    terraform

--- instance.tf
+++ instance.tf
@@ -3,3 +3,9 @@ resource "aws_iam_instance_profile" "sandy_profile" {
   role = aws_iam_role.role.name
 }
 
+
+data "aws_launch_template" "centos" {
+  name = "centos"
+  id = "lt-06a0f28ee3faf1441"
+}
+
