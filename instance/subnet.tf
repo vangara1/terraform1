80f18f1daaf67c469803cdaf5a3a91043b6fbc9f
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 17:52:36 +0530

    terraform

--- instance/subnet.tf
+++ instance/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "sandy" {
   vpc_id                                      = aws_vpc.sandy.id
-  cidr_block                                  = "10.0.1.0/24"
+  cidr_block                                  = "10.0.0.0/17"
   availability_zone                           = "us-east-1a"
   map_public_ip_on_launch                     = true
   enable_resource_name_dns_a_record_on_launch = true
--- instance/vpc.tf
+++ instance/vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "sandy" {
-  cidr_block       = "10.0.1.0/16"
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
