938e0d693a19386e3a918d235d6092d230105996
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 17:41:14 +0530

    terraform

--- instance/subnet.tf
+++ instance/subnet.tf
@@ -28,13 +28,15 @@ resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.sandy.id
 
   route {
-    cidr_block = "10.0.2.0/24"
+    cidr_block = "10.0.0.0/16"
     gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
     Name = "rt"
   }
 }
+
+
 
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.sandy.id
