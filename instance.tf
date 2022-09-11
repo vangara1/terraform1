c93941f66f2aa5dd073be041201e783d4bcb1e97
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 13:10:51 +0530

    terraform

--- instance.tf
+++ instance.tf
@@ -1,23 +1,25 @@
-#
-#resource "aws_instance" "foo" {
-#  ami           = "ami-005e54dee72cc1d00" # us-west-2
-#  instance_type = "t2.micro"
-#
-#  network_interface {
-#    network_interface_id = aws_network_interface.foo.id
-#    device_index         = 0
-#  }
-#
-#  credit_specification {
-#    cpu_credits = "unlimited"
-#  }
-#}
-#resource "aws_spot_instance_request" "cheap_worker" {
-#  ami           = "ami-1234"
-#  spot_price    = "0.03"
-#  instance_type = "c4.xlarge"
-#
-#  tags = {
-#    Name = "CheapWorker"
-#  }
-#}
+resource "aws_iam_instance_profile" "sandy_profile" {
+  name = "sandy_profile"
+  role = aws_iam_role.role.name
+}
+
+resource "aws_iam_role" "role" {
+  name = "test_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
