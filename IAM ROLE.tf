84597babcc4f7a4a8bd918bb33814ac397326fc0
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 12:38:30 +0530

    terraform

--- IAM ROLE.tf
+++ IAM ROLE.tf
@@ -1,52 +1,23 @@
-resource "aws_iam_role" "sunny" {
-  name = "sunny"
+resource "aws_iam_role" "role" {
+  name = "sandy_role"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Action": "ec2:*",
-        "Effect": "Allow",
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "elasticloadbalancing:*",
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "cloudwatch:*",
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "autoscaling:*",
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "iam:CreateServiceLinkedRole",
-        "Resource": "*",
-        "Condition": {
-          "StringEquals": {
-            "iam:AWSServiceName": [
-              "autoscaling.amazonaws.com",
-              "ec2scheduled.amazonaws.com",
-              "elasticloadbalancing.amazonaws.com",
-              "spot.amazonaws.com",
-              "spotfleet.amazonaws.com",
-              "transitgateway.amazonaws.com"
-            ]
-          }
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
         }
-      }
+      },
     ]
   })
 
   tags = {
-    tag-key = "sandy-admin"
+    tag-key = "role"
   }
 }
