b0dbec7351bb63182c3ecb9b326bc63ba833e881
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 11:02:36 +0530

    terraform

--- provider.tf
+++ provider.tf
@@ -1,52 +1,14 @@
-resource "aws_iam_role" "sunny" {
-  name = "sunny"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "ec2:*",
-        "Effect": "Allow",
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "elasticloadbalancing:*",
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "cloudwatch:*",
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "autoscaling:*",
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "iam:CreateServiceLinkedRole",
-        "Resource": "*",
-        "Condition": {
-          "StringEquals": {
-            "iam:AWSServiceName": [
-              "autoscaling.amazonaws.com",
-              "ec2scheduled.amazonaws.com",
-              "elasticloadbalancing.amazonaws.com",
-              "spot.amazonaws.com",
-              "spotfleet.amazonaws.com",
-              "transitgateway.amazonaws.com"
-            ]
-          }
-        }
-      }
-    ]
-  })
-
-  tags = {
-    tag-key = "sandy-admin"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
