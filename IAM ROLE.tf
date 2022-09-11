d94b45255368e1d57d356a02e24e64070c64a63b
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 12:51:29 +0530

    terraform

--- IAM ROLE.tf
+++ IAM ROLE.tf
@@ -1,42 +1,38 @@
-#resource "aws_iam_role" "role" {
-#  name = "sandy_role"
-#
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Sid    = ""
-#        Principal = {
-#          Service = "ec2.amazonaws.com" , "s3.amazonaws.com"
-#        }
-#      },
-#    ]
-#  })
-#
-#  tags = {
-#    tag-key = "role"
-#  }
-#}
-
-
 resource "aws_iam_role" "role" {
-  name = "instance-role"
+  name = "instance_role"
 
-  assume_role_policy = <<EOF
-  {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com", "s3.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-  EOF
-}
+    ]
+  })
+  }
+
+
+#resource "aws_iam_role" "role" {
+#  name = "instance-role"
+#
+#  assume_role_policy = <<EOF
+#  {
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Action": "sts:AssumeRole",
+#      "Principal": {
+#        "Service": "ec2.amazonaws.com", "s3.amazonaws.com"
+#      },
+#      "Effect": "Allow",
+#      "Sid": ""
+#    }
+#  ]
+#}
+#  EOF
+#}
