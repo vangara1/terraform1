65086d7ecd040caceae39d201c21cfd8f76e1621
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 15:31:32 +0530

    terraform

--- role/instance.tf
+++ role/instance.tf
@@ -14,3 +14,16 @@ resource "aws_instance" "role" {
     Name = "role"
   }
 }
+resource "aws_key_pair" "key" {
+  key_name   = "key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "file" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "file"
+}
