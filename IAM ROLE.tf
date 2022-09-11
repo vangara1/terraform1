3cebaaba7ce91863976ed9507dd7dfd83ea305c9
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 12:56:17 +0530

    terraform

--- IAM ROLE.tf
+++ IAM ROLE.tf
@@ -20,23 +20,51 @@ resource "aws_iam_role" "role" {
   }
 }
 
+resource "aws_iam_role_policy" "policy" {
+  name = "sandy_policy"
+  role = aws_iam_role.role.id
 
-#resource "aws_iam_role" "role" {
-#  name = "instance-role"
-#
-#  assume_role_policy = <<EOF
-#  {
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Action": "sts:AssumeRole",
-#      "Principal": {
-#        "Service": "ec2.amazonaws.com", "s3.amazonaws.com"
-#      },
-#      "Effect": "Allow",
-#      "Sid": ""
-#    }
-#  ]
-#}
-#  EOF
-#}
+   policy = jsonencode({
+     "Version": "2012-10-17",
+     "Statement": [
+   {
+     "Action": "ec2:*",
+     "Effect": "Allow",
+     "Resource": "*"
+   },
+   {
+     "Effect": "Allow",
+     "Action": "elasticloadbalancing:*",
+     "Resource": "*"
+   },
+   {
+     "Effect": "Allow",
+     "Action": "cloudwatch:*",
+     "Resource": "*"
+   },
+   {
+     "Effect": "Allow",
+     "Action": "autoscaling:*",
+     "Resource": "*"
+   },
+   {
+     "Effect": "Allow",
+     "Action": "iam:CreateServiceLinkedRole",
+     "Resource": "*",
+     "Condition": {
+     "StringEquals": {
+     "iam:AWSServiceName": [
+     "autoscaling.amazonaws.com",
+     "ec2scheduled.amazonaws.com",
+     "elasticloadbalancing.amazonaws.com",
+     "spot.amazonaws.com",
+     "spotfleet.amazonaws.com",
+     "transitgateway.amazonaws.com"
+   ]
+   }
+   }
+   }
+   ]
+  })
+}
+
