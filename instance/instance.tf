f2e42aaca37b617a73587af7d663494cf6f2c7f2
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 18:35:44 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "instance" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
-  key           = "terra"
+  key_name          = "terra"
   user_data = "${file("install_jenkins.sh")}"
 
   tags = {
