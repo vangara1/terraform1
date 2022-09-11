cbdb30a0f4fc394f6e3239b1d35b693a5a1748c6
vangara1 <vangarasandeep1996@gmail.com>
2022-09-11 15:18:47 +0530

    terraform

--- role/instance.tf
+++ role/instance.tf
@@ -14,9 +14,3 @@ resource "aws_instance" "role" {
     Name = "role"
   }
 }
-
-#resource "aws_key_pair" "terra" {
-#  key_name   = "terra"
-#
-#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCLySsXaB2TJzq5NrShfFkYh6kPv6A75jX7TkFPilH5OUGo3X12jRs483HE2UbDy4MPqld3hK0bH8kaIloYG4LgU8FR9D+Hc0JbHI2bq/FoPFSODu+VISBnwu5kEiWkdoTEMMyZPqhItluERfE1x9TvBqZ4Uk54heQBLE6BC5RQka08VKZwxV9VIBaZliB58QMBk2M7pOrOB4EWPs5x3Nxo2GUYajQX07YFk234fQyQAWcuBZGA8gHVMiY9Y7hS8ppPKF2vT8KvioyudoCb2AJQz1ItzJBUqUZXDz0oEppbYkxPZxJyQ01zMQumRpQPbCRdT0UrLn6KZXpzfBu/GwN7 imported-openssh-key"
-#}
