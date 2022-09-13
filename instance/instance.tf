c79ba0a3dc837dca1d531090586d4ffc4b213318
vangara1 <vangarasandeep1996@gmail.com>
2022-09-13 23:35:37 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -24,7 +24,8 @@ resource "aws_instance" "instance" {
 
 resource "null_resource" "jenkins" {
   provisioner "local-exec" {
-    command  = [
+    type = [ "shell" ]
+    inline  = [
       "sudo yum install -y jenkins java-11-openjdk-devel", "sudo yum -y install wget",
       "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
       "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key", "sudo yum upgrade -y",
