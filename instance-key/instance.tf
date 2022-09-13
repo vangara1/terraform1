e73ee681b9046a31d333f07cf0d33c9bc8b9f692
vangara1 <vangarasandeep1996@gmail.com>
2022-09-13 21:03:33 +0530

    terraform

--- instance-key/instance.tf
+++ instance-key/instance.tf
@@ -16,9 +16,7 @@ provider "aws" {
 resource "aws_instance" "instance" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
-  key_name          = "key"
-
-  tags = {
+    tags = {
     Name = "instance"
   }
 }
