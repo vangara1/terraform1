3b970091058260ea971d8a0c64e9edc229c737ed
vangara1 <vangarasandeep1996@gmail.com>
2022-09-13 23:44:45 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -16,18 +16,44 @@ provider "aws" {
 resource "aws_instance" "instance" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
-  key_name      = "terra"
+  key_name      = "key"
   tags          = {
     Name = "instance"
   }
 }
 
+provisioner "remote-exec" {
+  inline = [
+    "sudo yum install -y jenkins java-11-openjdk-devel",
+    "sudo yum -y install wget",
+    "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+    "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+    "sudo yum upgrade -y",
+    "sudo yum install jenkins -y",
+    "sudo systemctl start jenkins",
+  ]
+}
+connection {
+  type        = "ssh"
+  host        = aws_subnet.subnet.cidr_block
+  user        = "centos"
+  private_key = file("/home/centos/instance/key.pem")
+}
 
-  provisioner "local-exec" {
-    inline  = [
-      "sudo yum install -y jenkins java-11-openjdk-devel", "sudo yum -y install wget",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key", "sudo yum upgrade -y",
-      "sudo yum install jenkins -y", "sudo systemctl start jenkins",
-    ]
-  }
+tags = {
+  "Name"      = "Jenkins"
+}
+
+
+resource "aws_key_pair" "key" {
+key_name   = "key"
+public_key = tls_private_key.rsa.public_key_openssh
+}
+resource "tls_private_key" "rsa" {
+algorithm = "RSA"
+rsa_bits  = 4096
+}
+resource "local_file" "key" {
+content = tls_private_key.rsa.private_key_pem
+filename = "key.pem"
+}
