26e9fc44a5e32bd9d61d0cdc30f1a33628a425fd
vangara1 <vangarasandeep1996@gmail.com>
2022-09-13 21:03:58 +0530

    terraform

--- instance-key/instance.tf
+++ instance-key/instance.tf
@@ -29,7 +29,7 @@ resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-resource "local_file" "tf-key" {
+resource "local_file" "key" {
   content = tls_private_key.rsa.private_key_pem
-  filename = "tf-key"
+  filename = "key"
 }
