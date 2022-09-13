a08199b69d056703bec3618269ae987df9f2774f
vangara1 <vangarasandeep1996@gmail.com>
2022-09-13 23:00:00 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_instance" "instance" {
   ami             = "ami-002070d43b0a4f171"
-  vpc_security_group_ids = [aws_security_group.SG.id]
+  security_groups = [ aws_security_group.SG.id ]
   instance_type   = "t2.micro"
   key_name        = "terra"
   user_data       = "${file("install_jenkins.sh")}"
