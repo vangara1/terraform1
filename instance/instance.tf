507c9df91bf1c94e9eb167d495b2f3cd41e5e5c1
vangara1 <vangarasandeep1996@gmail.com>
2022-09-13 23:25:12 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -14,23 +14,21 @@ provider "aws" {
 
 
 resource "aws_instance" "instance" {
-  ami             = "ami-002070d43b0a4f171"
+  ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
   key_name      = "terra"
-  user_data = <<EOF
-#! /bin/bash
-sudo yum update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum update -y
-sudo yum install jenkins java-1.8.0-openjdk-devel -y
-sudo systemctl daemon-reload
-sudo systemctl start jenkins && sudo systemctl status jenkins
-EOF
+  tags          = {
+    Name = "instance"
+  }
+}
 
-
-tags = {
-    Name = "TRIAL"
+resource "null_resource" "jenkins" {
+  provisioner "local-exec" {
+    inline = [
+      "sudo yum install -y jenkins java-11-openjdk-devel", "sudo yum -y install wget",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key", "sudo yum upgrade -y",
+      "sudo yum install jenkins -y", "sudo systemctl start jenkins",
+    ]
   }
 }
