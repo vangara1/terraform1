b6f52ee841ec0ed23d2963bf28173d6c5f6cb8ad
vangara1 <vangarasandeep1996@gmail.com>
2022-09-13 23:36:57 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -17,6 +17,12 @@ resource "aws_instance" "instance" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
   key_name      = "terra"
+  tags          = {
+    Name = "instance"
+  }
+}
+
+
   provisioner "local-exec" {
     inline  = [
       "sudo yum install -y jenkins java-11-openjdk-devel", "sudo yum -y install wget",
@@ -25,10 +31,3 @@ resource "aws_instance" "instance" {
       "sudo yum install jenkins -y", "sudo systemctl start jenkins",
     ]
   }
-
-  tags          = {
-    Name = "instance"
-  }
-}
-
-
