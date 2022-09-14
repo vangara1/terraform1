f20052d2a34b5b27fcbf4b35ac6729845fc6d3c3
vangara1 <vangarasandeep1996@gmail.com>
2022-09-14 13:45:37 +0530

    terraform

--- instance/sg.tf
+++ instance/sg.tf
@@ -3,35 +3,41 @@ resource "aws_security_group" "security" {
   description = "Allow TLG  inbound traffic"
   vpc_id      = aws_vpc.sandy.id
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [aws_subnet.subnet.cidr_block]
-  }
 #  ingress {
 #    description = "TLS from VPC"
-#    from_port   = 22
-#    to_port     = 22
+#    from_port   = 443
+#    to_port     = 443
+#    protocol    = "tcp"
+#    cidr_blocks = [aws_subnet.subnet.cidr_block]
+#  }
+##  ingress {
+##    description = "TLS from VPC"
+##    from_port   = 22
+##    to_port     = 22
+##    protocol    = "tcp"
+##    cidr_blocks = [aws_subnet.subnet.cidr_block]
+##  }
+#  ingress {
+#    description = "TLS from VPC"
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = [aws_subnet.subnet.cidr_block]
+#  }
+#  ingress {
+#    description = "TLS from VPC"
+#    from_port   = 8080
+#    to_port     = 8080
 #    protocol    = "tcp"
 #    cidr_blocks = [aws_subnet.subnet.cidr_block]
 #  }
   ingress {
-    description = "TLS from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [aws_subnet.subnet.cidr_block]
+    description = "Allowing all ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = [aws_subnet.subnet.cidr_block]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
