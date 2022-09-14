e9cdf7a690eceafbe5c4448243a04e393dbcb594
vangara1 <vangarasandeep1996@gmail.com>
2022-09-14 13:36:17 +0530

    terraform

--- instance/sg.tf
+++ instance/sg.tf
@@ -8,28 +8,28 @@ resource "aws_security_group" "security" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.sandy.cidr_block]
+    cidr_blocks = [aws_subnet.subnet.id]
   }
   ingress {
     description = "TLS from VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.sandy.cidr_block]
+    cidr_blocks = [aws_subnet.subnet.id]
   }
   ingress {
     description = "TLS from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.sandy.cidr_block]
+    cidr_blocks = [aws_subnet.subnet.id]
   }
   ingress {
     description = "TLS from VPC"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.sandy.cidr_block]
+    cidr_blocks = [aws_subnet.subnet.id]
   }
 
   egress {
