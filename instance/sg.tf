95837537fae2b54df366e90fdab5070ddaa180f4
vangara1 <vangarasandeep1996@gmail.com>
2022-09-14 12:58:04 +0530

    terraform

--- instance/sg.tf
+++ instance/sg.tf
@@ -48,8 +48,8 @@ resource "aws_security_group" "security" {
 #output "aws_security_group" {
 #  value = aws_security_group.SG.id
 #}
-
-resource "aws_network_interface_sg_attachment" "sg_attachment" {
-  security_group_id    = aws_security_group.security.id
-  network_interface_id = aws_instance.instance.primary_network_interface_id
-}
+#
+#resource "aws_network_interface_sg_attachment" "sg_attachment" {
+#  security_group_id    = aws_security_group.security.id
+#  network_interface_id = aws_instance.instance.primary_network_interface_id
+#}
