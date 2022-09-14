56a8d7de600a528dd80ef133d84e4bfb37ddac8c
vangara1 <vangarasandeep1996@gmail.com>
2022-09-14 14:10:45 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -12,13 +12,13 @@ resource "aws_instance" "instance" {
         sudo yum update -y
         sudo yum install -y yum-utils
         sudo yum -y install epel-release
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
-sudo yum install java-11-openjdk -y
-sudo yum install jenkins -y
-sudo systemctl daemon-reload
+        sudo wget -O /etc/yum.repos.d/jenkins.repo \
+            https://pkg.jenkins.io/redhat-stable/jenkins.repo
+        sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+        sudo yum upgrade -y
+        sudo yum install java-11-openjdk -y
+        sudo yum install jenkins -y
+        sudo systemctl daemon-reload
         sudo systemctl enable jenkins
         sudo systemctl start jenkins
         sudo systemctl status jenkins
