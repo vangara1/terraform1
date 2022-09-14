1c044ee49c8504af587b04cbe2600501bfaf073c
vangara1 <vangarasandeep1996@gmail.com>
2022-09-14 12:13:10 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "instance" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
   key_name      = "terra"
-  vpc_security_group_ids = aws_security_group.SG.*.id
+  security_group= aws_security_group.SG.*.id
   #  wait_for_fulfillment = true
   tags          = {
     Name = "instance"
