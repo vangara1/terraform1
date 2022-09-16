b508682a850aa086efc27239b6c5a726f6b35976
vangara1 <vangarasandeep1996@gmail.com>
2022-09-16 18:04:52 +0530

    terraform

--- instance/sg.tf
+++ instance/sg.tf
@@ -8,21 +8,21 @@ resource "aws_security_group" "security" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    prefix_list_ids = [0.0.0.0/0]
+    prefix_list_ids = ["0.0.0.0/0"]
   }
   ingress {
     description = "TLS from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "TLS from VPC"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
