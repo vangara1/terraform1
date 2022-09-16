322f436b3ddc37b712b60ea7a1c71d4e8c094206
vangara1 <vangarasandeep1996@gmail.com>
2022-09-16 19:20:07 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -7,7 +7,19 @@ resource "aws_instance" "instance" {
   associate_public_ip_address = true
 
 
-    user_data = file("install_jenkins.sh")
+  provisioner "local-exec" {
+    command = <<-EOT
+#!/bin/bash
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum update -y
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins && sudo systemctl status jenkins
+    EOT
+  }
 
   tags = {
     Name = "${var.name}-instance"
