d3913419ff4e6d2848197bd3d45e0b4fbea20186
vangara1 <vangarasandeep1996@gmail.com>
2022-09-16 22:03:54 +0530

    terraform

--- auto-scale/sg.tf
+++ auto-scale/sg.tf
@@ -1,19 +1,23 @@
-module "vote_service_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "terra-sg-service"
-  description = "Security group with custom ports open within VPC"
-  vpc_id      = var.vpc_id
-
-  ingress_cidr_blocks      = ["0.0.0.0/0"]
-  ingress_rules            = ["tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "tcp"
-      description = " ports"
-      cidr_blocks = "10.10.0.0/16"
-    }
-  ]
-}
+#module "vote_service_sg" {
+#  source = "terraform-aws-modules/security-group/aws"
+#
+#  name        = "sg-service"
+#  description = "Security group with custom ports open within VPC"
+#  vpc_id      = var.vpc_id
+#
+#  ingress_cidr_blocks      = ["0.0.0.0/0"]
+#  ingress_rules            = ["https-443-tcp"]
+#  ingress_with_cidr_blocks = [
+#    {
+#      from_port   = 8080
+#      to_port     = 8090
+#      protocol    = "tcp"
+#      description = "User-service ports"
+#      cidr_blocks = "10.10.0.0/16"
+#    },
+#    {
+#      rule        = "postgresql-tcp"
+#      cidr_blocks = "0.0.0.0/0"
+#    },
+#  ]
+#}
