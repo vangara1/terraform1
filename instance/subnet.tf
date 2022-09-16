a5f33efa22a7fdd0b05b8ab1ff5456b937cd31a2
vangara1 <vangarasandeep1996@gmail.com>
2022-09-16 20:41:04 +0530

    terraform

--- instance/subnet.tf
+++ instance/subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "subnet" {
   vpc_id                                      = aws_vpc.sandy.id
-  availability_zone                           = var.az
-  cidr_block                                  = var.subnet_cidr
+  availability_zone                           = var.az[0]
+  cidr_block                                  = var.subnet_cidr[0]
   enable_resource_name_dns_a_record_on_launch = "true"
   map_public_ip_on_launch                     = "true"
 
@@ -11,11 +11,17 @@ resource "aws_subnet" "subnet" {
 }
 
 
+resource "aws_subnet" "subnet-1" {
+  vpc_id                                      = aws_vpc.sandy.id
+  availability_zone                           = var.az[1]
+  cidr_block                                  = var.subnet_cidr[1]
+  enable_resource_name_dns_a_record_on_launch = "true"
+  map_public_ip_on_launch                     = "true"
 
-
-
-
-
+  tags = {
+    Name = "${var.name}-subnet-1"
+  }
+}
 
 
 
