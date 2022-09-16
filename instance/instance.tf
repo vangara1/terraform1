37a0831d0333d30d4d3fc52d5e6ac19c24aa7adc
vangara1 <vangarasandeep1996@gmail.com>
2022-09-16 19:25:40 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "instance" {
   provisioner "local-exec" {
     command = <<-EOT
 #!/bin/bash
-sudo yum update -y
+sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
