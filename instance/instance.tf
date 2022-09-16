36bf66166a7170570f8ea74f06a28c85ddbf279e
vangara1 <vangarasandeep1996@gmail.com>
2022-09-16 18:58:22 +0530

    terraform

--- instance/instance.tf
+++ instance/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami                         = var.ami
   instance_type               = var.instance
-#  key_name                    = "key"
+  key_name                    = var.inst-key
   vpc_security_group_ids      = [aws_security_group.security.id]
   subnet_id                   = aws_subnet.subnet.id
   associate_public_ip_address = true
